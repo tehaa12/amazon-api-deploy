6f6a89326b82f033197f77136f94b524443b8f1a
teha <tehaabdurahman@gmail.com>
2024-10-09 11:39:29 -0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // Use Stripe secret key from environment variable
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 // Initialize Express
 const app = express();
